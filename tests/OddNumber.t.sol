416f14989225bbfcf1703f6724ae2673b81807a9
Pablo Bisceglia <86027276+pb-forte@users.noreply.github.com>
2025-02-17 11:16:33 -0300

    Update OddNumber.t.sol

--- tests/OddNumber.t.sol
+++ tests/OddNumber.t.sol
@@ -29,6 +29,6 @@ contract OddNumberTest is RiscZeroCheats, Test {
         (bytes memory journal, bytes memory seal) = prove(Elf.IS_ODD_PATH, abi.encode(number));
 
         oddNumber.set(abi.decode(journal, (uint256)), seal);
-        assertNotEq(oddNumber.get(), number);
+        !assertEq(oddNumber.get(), number);
     }
 }
