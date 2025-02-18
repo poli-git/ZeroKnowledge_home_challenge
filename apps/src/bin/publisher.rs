cf07851cc6367d3655411d59709e38d0c0a52fee
Pablo Bisceglia <86027276+pb-forte@users.noreply.github.com>
2025-02-18 19:19:31 -0300

    Validate OTL

--- apps/src/bin/publisher.rs
+++ apps/src/bin/publisher.rs
@@ -10,6 +10,16 @@ use risc0_ethereum_contracts::encode_seal;
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts, VerifierContext};
 use url::Url;
 
+
+use opentelemetry::{
+    global, runtime,
+    sdk::{propagation::TraceContextPropagator, trace, Resource},
+    trace::{TraceContextExt, TraceError, Tracer},
+    Key, KeyValue,
+};
+use opentelemetry_otlp::WithExportConfig;
+use rand::Rng;
+
 // `IOddNumber` interface automatically generated via the alloy `sol!` macro.
 alloy::sol!(
     #[sol(rpc, all_derives)]
@@ -41,10 +51,80 @@ struct Args {
     input: U256,
 }
 
+
+// create a constant key
+const RANDOM: Key = Key::from_static_str("random.value");
+
+fn init_tracer() -> Result<trace::Tracer, TraceError> {
+    // Initialise OTLP Pipeline
+    opentelemetry_otlp::new_pipeline()
+        .tracing() // create OTLP tracing pipeline
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic() // create GRPC layer 
+                .with_endpoint("http://localhost:4317"), // GRPC OTLP Jaeger Endpoint
+        )
+        // Trace provider configuration 
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                "rust-otlp-basic",
+            )])),
+        )
+        .install_batch(runtime::Tokio) // configure a span exporter
+}
+
+
+
+fn gen_number() -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen()
+}
+
 fn main() -> Result<()> {
     env_logger::init();
     // Parse CLI Arguments: The application starts by parsing command-line arguments provided by the user.
     let args = Args::parse();
+
+
+    //
+
+     // set the Global Propagator
+     global::set_text_map_propagator(TraceContextPropagator::new());
+
+     // intialise the tracer
+     let tracer = init_tracer()?;
+ 
+     // start a new active span
+     tracer.in_span("generating number", |cx| {
+         let span = cx.span();
+         let num = gen_number();
+         span.add_event(
+             "opentel demo event Generating Number".to_string(),
+             vec![Key::new("number").i64(num.into())],
+         );
+ 
+         // set an active span attribute
+         span.set_attribute(RANDOM.i64(10));
+ 
+ 
+         // start a new span
+         tracer.in_span("generate another number", |cx| {
+             let span = cx.span();
+             let num = gen_number();
+             span.add_event(
+                 "Generating Number".to_string(),
+                 vec![Key::new("number").i64(num.into())],
+             )
+         })
+     });
+ 
+     // gracefully shutdown the tracer
+     global::shutdown_tracer_provider();
+
+
+    //
+
 
     // Create an alloy provider for that private key and URL.
     let wallet = EthereumWallet::from(args.eth_wallet_private_key);
