69a9fba5de019964c0913dc85c5442e82161c3f2
Pablo Bisceglia <86027276+pb-forte@users.noreply.github.com>
2025-02-18 17:24:03 -0300

    Added OpenTelemetry

--- apps/src/bin/publisher.rs
+++ apps/src/bin/publisher.rs
@@ -10,6 +10,15 @@ use risc0_ethereum_contracts::encode_seal;
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts, VerifierContext};
 use url::Url;
 
+use opentelemetry::{
+    global, runtime,
+    sdk::{propagation::TraceContextPropagator, trace, Resource},
+    trace::{TraceContextExt, TraceError, Tracer},
+    Key, KeyValue,
+};
+use opentelemetry_otlp::WithExportConfig;
+
+
 // `IOddNumber` interface automatically generated via the alloy `sol!` macro.
 alloy::sol!(
     #[sol(rpc, all_derives)]
@@ -41,8 +50,35 @@ struct Args {
     input: U256,
 }
 
-fn main() -> Result<()> {
+fn init_tracer() -> Result<trace::Tracer, TraceError> {
+    // Initialise OTLP Pipeline
+    opentelemetry_otlp::new_pipeline()
+        .tracing() // create OTLP tracing pipeline
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic() // create GRPC layer 
+                .with_endpoint("http://localhost:4317"), // GRPC OTLP Jaeger Endpoint
+        )
+        // Trace provider configuration 
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                "rust-otlp-basic",
+            )])),
+        )
+        .install_batch(runtime::Tokio) // configure a span exporter
+}
+
+fn main() -> Result<(), TraceError> {
     env_logger::init();
+
+    // set the Global Propagator
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    // intialise the tracer
+    let tracer = init_tracer().unwrap();
+
+
     // Parse CLI Arguments: The application starts by parsing command-line arguments provided by the user.
     let args = Args::parse();
 
@@ -52,6 +88,20 @@ fn main() -> Result<()> {
         .with_recommended_fillers()
         .wallet(wallet)
         .on_http(args.rpc_url);
+
+
+    // start a new active span
+    tracer.in_span("Create an alloy provider ", |cx| {
+        let span = cx.span();
+        
+        span.add_event(
+            "Creating provider".to_string(),
+            vec![args.rpc_url],
+        );       
+    });
+
+    // gracefully shutdown the tracer
+    global::shutdown_tracer_provider();
 
     // ABI encode input: Before sending the proof request to the Bonsai proving service,
     // the input number is ABI-encoded to match the format expected by the guest code running in the zkVM.
